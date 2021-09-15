21b6cd3716575300f3cdd3e1f05e0ce21c6698c6
Nicolas Beddows <nicolas.beddows@gmail.com>
2021-09-15 15:55:43 +1000

    Added try catch block around main function block.

--- source/main.cpp
+++ source/main.cpp
@@ -15,33 +15,40 @@ using namespace Emulator;
 
 int main(void)
 {
-	//The machine to run Space Invaders on.
-	auto machine = MakeMachine();
-	//Create our custom Space Invaders I/O controller.
-	auto ioController = std::make_unique<IoController>(machine->ControlBus());
-	//The default memory controller is sufficient for demonstration purposes.
-	auto memoryController = MakeDefaultMemoryController(16);
+	try
+	{
+		//The machine to run Space Invaders on.
+		auto machine = MakeMachine();
+		//Create our custom Space Invaders I/O controller.
+		auto ioController = std::make_unique<IoController>(machine->ControlBus());
+		//The default memory controller is sufficient for demonstration purposes.
+		auto memoryController = MakeDefaultMemoryController(16);
 
-	/*
-		Load the ROM into memory, the layout
-		is as follows:
+		/*
+			Load the ROM into memory, the layout
+			is as follows:
 
-			invaders-h 0000-07FF
-			invaders-g 0800-0FFF
-			invaders-f 1000-17FF
-			invaders-e 1800-1FFF
-	*/
-	memoryController->Load("../roms/invaders-h.bin", 0x0000);
-	memoryController->Load("../roms/invaders-g.bin", 0x0800);
-	memoryController->Load("../roms/invaders-f.bin", 0x1000);
-	memoryController->Load("../roms/invaders-e.bin", 0x1800);
-	
-	//Load our controllers into the machine.
-	machine->SetMemoryController(std::move(memoryController));
-	machine->SetIoController(std::move(ioController));
-	
-	//Only returns when signalled by an I/O device, for our demonstration it will be after 10 seconds.
-	machine->Run(0x00);
+				invaders-h 0000-07FF
+				invaders-g 0800-0FFF
+				invaders-f 1000-17FF
+				invaders-e 1800-1FFF
+		*/
+		memoryController->Load("../roms/invaders-h.bin", 0x0000);
+		memoryController->Load("../roms/invaders-g.bin", 0x0800);
+		memoryController->Load("../roms/invaders-f.bin", 0x1000);
+		memoryController->Load("../roms/invaders-e.bin", 0x1800);
+
+		//Load our controllers into the machine.
+		machine->SetMemoryController(std::move(memoryController));
+		machine->SetIoController(std::move(ioController));
+
+		//Only returns when signalled by an I/O device, for our demonstration it will be after 10 seconds.
+		machine->Run(0x00);
+	}
+	catch (std::exception e)
+	{
+		printf ("%s\n", e.what());
+	}
 
 	return 0;
 }
