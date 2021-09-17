0fe9db41dc0e1fdd830e565cdcd55d4b96e7b59d
Nicolas Beddows <nicolas.beddows@gmail.com>
2021-09-17 16:00:38 +1000

    Removed io controller read documents and formally put them in the module.

--- source/SpaceInvaders.cpp
+++ source/SpaceInvaders.cpp
@@ -116,45 +116,6 @@ namespace SpaceInvaders
 
 	uint8_t IoController::Read(uint16_t port)
 	{
-		/*
-			Read the value from the input device (keyboard for example)
-			and set the relevant bit in the return value according to
-			the following:
-
-			Port 0
-				 bit 0 DIP4 (Seems to be self-test-request read at power up)
-				 bit 1 Always 1
-				 bit 2 Always 1
-				 bit 3 Always 1
-				 bit 4 Fire
-				 bit 5 Left
-				 bit 6 Right
-				 bit 7 ? tied to demux port 7 ?
-
-			Port 1
-				 bit 0 = CREDIT (1 if deposit)
-				 bit 1 = 2P start (1 if pressed)
-				 bit 2 = 1P start (1 if pressed)
-				 bit 3 = Always 1
-				 bit 4 = 1P shot (1 if pressed)
-				 bit 5 = 1P left (1 if pressed)
-				 bit 6 = 1P right (1 if pressed)
-				 bit 7 = Not connected
-
-			Port 2
-				 bit 0 = DIP3 00 = 3 ships  10 = 5 ships
-				 bit 1 = DIP5 01 = 4 ships  11 = 6 ships
-				 bit 2 = Tilt
-				 bit 3 = DIP6 0 = extra ship at 1500, 1 = extra ship at 1000
-				 bit 4 = P2 shot (1 if pressed)
-				 bit 5 = P2 left (1 if pressed)
-				 bit 6 = P2 right (1 if pressed)
-				 bit 7 = DIP7 Coin info displayed in demo screen 0=ON
-
-			Port 3
-				bit 0-7 Shift register data
-
-		*/
 		switch (port)
 		{
 			case 0:
