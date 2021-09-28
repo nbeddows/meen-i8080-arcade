83fd502667f0f1449a720bd7c3cff49899d015f4
Nicolas Beddows <nicolas.beddows@gmail.com>
2021-09-28 13:06:16 +1000

    Added SDL Window support for rendering.

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,14 @@
+#include <exception>
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+
 /*
 	Import the required modules
 
 	SpaceInvaders:		custom module specifically targetting the Space Invaders ROM.
 	MachineFactory:		module for running our custom Space Invaders machine.
 */
+import <memory>;
 import SpaceInvaders;
 import MachineFactory;
 
@@ -15,12 +20,33 @@ int main(void)
 {
 	try
 	{
+		SDL_SetMainReady();
+
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			throw; //std::runtime_error();
+		}
+
+		auto window = std::shared_ptr<SDL_Window>(SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 256, 224, 0), [](SDL_Window* w) { SDL_DestroyWindow(w); });
+
+		if (window == nullptr)
+		{
+			throw std::bad_alloc();
+		}
+
+		auto renderer = std::shared_ptr <SDL_Renderer>(SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_ACCELERATED), [](SDL_Renderer* r) { SDL_DestroyRenderer(r); });
+
+		if (renderer == nullptr)
+		{
+			throw std::bad_alloc();
+		}
+
 		//The machine to run Space Invaders on.
 		auto machine = MakeMachine();
-		//Create our custom Space Invaders I/O controller.
-		auto ioController = std::make_shared<IoController>();
 		//Create our custom Space Invaders memory controller.
-		auto memoryController = std::make_shared<MemoryController>(16);
+		auto memoryController = std::make_shared<MemoryController>(16, renderer);
+		//Create our custom Space Invaders I/O controller.
+		auto ioController = std::make_shared<IoController>(memoryController);
 
 		/*
 			Load the ROM into memory, the layout
@@ -36,12 +62,19 @@ int main(void)
 		memoryController->Load("../roms/invaders-f.bin", 0x1000);
 		memoryController->Load("../roms/invaders-e.bin", 0x1800);
 
+		//memoryController->Load("../roms/INVADERS.H", 0x0000);
+		//memoryController->Load("../roms/INVADERS.G", 0x0800);
+		//memoryController->Load("../roms/INVADERS.F", 0x1000);
+		//memoryController->Load("../roms/INVADERS.E", 0x1800);
+
 		//Load our controllers into the machine.
 		machine->SetMemoryController(memoryController);
 		machine->SetIoController(ioController);
 
-		//Only returns when signalled by an I/O device, for our demonstration it will be after 10 seconds.
+		//Only returns when signalled by an I/O device, for our demonstration it will be when the 'q' key is pressed.
 		machine->Run(0x00);
+
+		SDL_Quit();
 	}
 	catch (std::exception e)
 	{
