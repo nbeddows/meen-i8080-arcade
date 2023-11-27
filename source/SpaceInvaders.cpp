1e89b8e2e1d8d0637ddd451ed7a84d16540cb674
Nicolas Beddows <nicolas.beddows@gmail.com>
2023-11-28 10:26:58 +1100

    chore: add MIT license

--- source/SpaceInvaders.cpp
+++ source/SpaceInvaders.cpp
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2021-2023 Nicolas Beddows <nicolas.beddows@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 module;
 
 #include <assert.h>
@@ -294,7 +316,6 @@ namespace SpaceInvaders
 				{
 					if (state[scanCode] != 0)
 					{
-						//printf("%s\n", str);
 						ret |= bit;
 					}
 				};
@@ -351,8 +372,7 @@ namespace SpaceInvaders
 					[[maybe_unused]] auto busy = Mix_PlayChannel(-1 /* use the next available channel */, mixChunk_[i], 0 /* don't loop (play it once) */);
 					// We are playing 8 (default maximum) samples at the same time, this should not happen!
 					// We are trying to play a track which isn't loaded (an unknown data bit is set?!?!)
-					//assert(mixChunk_ != nullptr && busy != -1);
-
+					//assert(mixChunk_[i] == nullptr || busy != -1);
 				}
 			}
 		}
