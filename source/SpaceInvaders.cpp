7a1e9f61201325ffda06dff773d470c33b4505e8
Nicolas Beddows <nicolas.beddows@gmail.com>
2024-02-02 15:30:29 +1100

    docs: update copyright

--- source/SpaceInvaders.cpp
+++ source/SpaceInvaders.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021-2023 Nicolas Beddows <nicolas.beddows@gmail.com>
+Copyright (c) 2021-2024 Nicolas Beddows <nicolas.beddows@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
