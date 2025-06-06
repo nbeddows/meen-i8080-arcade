b0e8f9feee3876886e4d59b5d79163a530679737
Nicolas Beddows <nicolas.beddows@gmail.com>
2025-06-06 11:07:49 +1000

    fix: compute the audio sample size based on the a/v frame rates

--- source/SdlIoController.cpp
+++ source/SdlIoController.cpp
@@ -77,7 +77,13 @@ namespace i8080_arcade
 			printf("Failed to create i8080 arcade hardware");
 		}
 
-		if (Mix_OpenAudio(audioHardware["sampleRate"].as<int>(), 8 /* format (mono) */, audioHardware["channels"].as<int>(), audioHardware["sampleSize"].as<int>()) < 0)
+		int sampleRate = audioHardware["sampleRate"].as<int>();
+		// Allocate a sample buffer large enough to span one video frame duration.
+		// Note: depending on the sample rate this may not be a whole number and will be truncated,
+		// hence it could be one sample less than a video frame duration (this should be fine).
+		int sampleSize = sampleRate / 60; // 60 - video runs a 60hz
+
+		if (Mix_OpenAudio(sampleRate, 8 /* format (mono) */, audioHardware["channels"].as<int>(), sampleSize) < 0)
 		{
 			printf("Failed to open SDL Mixer");
 		}
@@ -423,7 +429,7 @@ namespace i8080_arcade
 					e.user.data1 = reinterpret_cast<void*>(eventData);
 					e.user.data2 = reinterpret_cast<void*>(audio);
 					SDL_PushEvent(&e);
-				}	
+				}
 				else
 				{
 					// We have no audio to send, return the data back to the pool
